1b29d03b52bceec28f00c7ea40c0a5ac238e91e7
Dtan713 <s.donaldtan@gmail.com>
2024-05-14 15:44:45 -0400

    assignment

---
+++ index.js
@@ -0,0 +1,71 @@
+console.log("tasting control flow");
+
+
+// this is my bathroom
+/**
+ * scoping
+ * Block: a section of code thay seperated pieces of code in its own little enviorment
+ * function: 
+ * 
+ * 
+ * 
+ */
+{
+    const washHands = `sink`;
+    const lookAtYourself = "Mirror"
+}
+
+
+//create a function
+
+function addition (a , b){
+    
+    console.log(a + b);
+
+
+
+}
+
+addition(1, 3)  
+
+function calaculator(num1 , num2, operator){
+
+    if ( operator ==="+"){
+        console.log(num1 + num2);}
+
+    if (operator ==="-") {
+        console.log(num1 * num2);
+    }
+    if (operator=== "*") {
+        console.log(num1 * num2);
+
+    }
+    if (operator ==="/") {
+        console.log(num1 / num2);
+    }
+
+    
+    // take two numbers and also a operators
+}
+
+// calaculator(26, 64,"*");
+// calaculator(25., 75, "/");
+// calaculator(125, 50, "-");
+// calaculator(45, 56, "+");    
+
+const isSunny = false
+
+ (isSunny) {
+    console.log("time to run :)");{
+       else
+        console.log(" i will not go for a run");}
+
+    //switch case statement
+function calcTwo(num1, num2, operator){
+    switch (operator){
+     case"+":
+      console.log(num1 + num2)
+      break;
+    case "-":
+         console.log(num1 + num2)
+ }
